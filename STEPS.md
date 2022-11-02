1772d876457638d7b27b70ebad24e0abdd149892
brendan.sawyer@zoominfo.com <bjsawyer@vt.edu>
2022-11-02 14:19:30 -0400

    Prettier

--- STEPS.md
+++ STEPS.md
@@ -1,14 +1,14 @@
 # Plan (temp)
 
-- Online retailer app
-  - Components:
-    - Home
-      - Contains workshop steps
-    - Store
-      - User can select products & add to cart
-      - Utilize effects somehow
-        - Make some fake API request that checks inventory?
-    - Cart
-      - Shows selected products
-    - Navbar
-      - Shows pages & count of selected products
+-   Online retailer app
+    -   Components:
+        -   Home
+            -   Contains workshop steps
+        -   Store
+            -   User can select products & add to cart
+            -   Utilize effects somehow
+                -   Make some fake API request that checks inventory?
+        -   Cart
+            -   Shows selected products
+        -   Navbar
+            -   Shows pages & count of selected products
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,48 @@
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { RouterModule, Routes } from "@angular/router";
-import { AppComponent } from "./app.component";
-import { CartComponent } from "./cart/cart.component";
-import { HomeComponent } from "./home/home.component";
-import { MaterialModule } from "./material.module";
-import { ProductsComponent } from "./products/products.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { RouterModule, Routes } from '@angular/router'
+import { AppComponent } from './app.component'
+import { CartComponent } from './cart/cart.component'
+import { HomeComponent } from './home/home.component'
+import { MaterialModule } from './material.module'
+import { ProductsComponent } from './products/products.component'
 
 const ROUTES: Routes = [
-  {
-    path: "",
-    redirectTo: "home",
-  },
-  {
-    path: "home",
-    component: HomeComponent,
-  },
-  {
-    path: "products",
-    component: ProductsComponent,
-  },
-  {
-    path: "cart",
-    component: CartComponent,
-  },
-];
+    {
+        path: '',
+        redirectTo: 'home',
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        path: 'products',
+        component: ProductsComponent,
+    },
+    {
+        path: 'cart',
+        component: CartComponent,
+    },
+]
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    RouterModule.forRoot(ROUTES),
-  ],
-  declarations: [AppComponent, HomeComponent, ProductsComponent, CartComponent],
-  bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MaterialModule,
+        RouterModule.forRoot(ROUTES),
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        ProductsComponent,
+        CartComponent,
+    ],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
