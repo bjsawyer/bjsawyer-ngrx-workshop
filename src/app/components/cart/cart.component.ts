54e9b9369488f392b4614b75eaeb568f01eff893
brendan.sawyer@zoominfo.com <bjsawyer@vt.edu>
2022-11-03 09:53:43 -0400

    Cart enhancements

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core'
+import { IProduct } from 'app/services/product.service'
+import { ProductFacade } from 'app/state/product/product.facade'
+import { Observable } from 'rxjs'
 
 @Component({
   selector: 'app-cart',
@@ -6,7 +9,10 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-  constructor() {}
+  // TODO: Get items in cart from state
+  cart$: Observable<IProduct[]>
+
+  constructor(private _productFacade: ProductFacade) {}
 
   ngOnInit(): void {}
 }
--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -10,6 +10,7 @@ import { Observable, of } from 'rxjs'
 })
 export class ProductCardComponent {
   @Input() product: IProduct
+  @Input() view?: 'products' | 'cart' = 'products'
 
   constructor(private _productFacade: ProductFacade) {}
 
